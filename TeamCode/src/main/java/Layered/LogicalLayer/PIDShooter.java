7357f4afcdc048605afbe7d0d49f32aaa03a9b60
Soorya Balaji <soorya.balajivijayaraghavan@gmail.com>
2025-10-05 12:19:19 -0700

    PID Shooter V2 Has Projectile Calculations

--- TeamCode/src/main/java/Layered/LogicalLayer/PIDShooter.java
+++ TeamCode/src/main/java/Layered/LogicalLayer/PIDShooter.java
@@ -1,4 +1,4 @@
-package LogicalLayer;
+package Layered.LogicalLayer;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
